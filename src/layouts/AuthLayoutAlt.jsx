7830e13cd434df53bbf5e35199fd3ef80be7805c
Edward-Precious Omegbu <eomegbu@gmail.com>
2023-10-06 15:48:33 +0100

    AuthLayout most used one is done

--- src/layouts/AuthLayoutAlt.jsx
+++ src/layouts/AuthLayoutAlt.jsx
@@ -1,11 +1,11 @@
-import AuthHeader from "../sections/AuthHeader";
+import AuthHeaderAlt from "../sections/AuthHeaderAlt";
 import PropTypes from "prop-types";
 
 const AuthLayoutAlt = ({ children }) => {
 
     return (
         <div className="w-full bg-white h-screen">
-            <AuthHeader />
+            <AuthHeaderAlt />
             <main className="w-full container-108 pb-16 pt-4 lg:pt-16 h-authMainHeight flex items-start lg:items-center lg:justify-center">
                 {children}
             </main>
@@ -17,4 +17,4 @@ AuthLayoutAlt.propTypes = {
     children: PropTypes.element
 }
 
-export default AuthLayoutAlt
+export default AuthLayoutAlt;
