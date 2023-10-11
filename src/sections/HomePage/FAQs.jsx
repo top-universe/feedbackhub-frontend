516dc1967d063203552a3b356cd7ebe7514b661a
Tamunosaki <tamunosakijonah@gmail.com>
2023-10-11 15:20:26 +0100

    feedback second laning page

--- src/sections/HomePage/FAQs.jsx
+++ src/sections/HomePage/FAQs.jsx
@@ -1,8 +1,65 @@
+import question from '../../assets/images/question.png'
 const FAQs = () => {
 
     return (
-        <section className="container-108 bg-white">
-            FAQs Section
+        <section class="container-64 md:container-108 bg-white grid md:grid-cols-4">
+            <div class='md:col-span-3' >
+                <div class='mb-10'>
+                    <h1 class='font-bold text-xl md:text-3xl mb-1'>Frequently Asked Questions</h1>
+                    <p class='text-sm'>Got a question? We're here to answer! 
+                    f you.</p>
+                    <p class='text-sm'>don't see your question here, drop us a line on our <a href="#">contact page</a></p>
+                </div>
+                <div>
+                    <div class='flex justify-start md:justify-between text-lg mb-3 bg-bluegray-2 py-3 px-4'>
+                        <p>what is feedbackhub</p>
+                        <p>-</p>
+                    </div>
+                    <p class='py-3 px-4 text-base'>FeedbackHub is an innovative customer feedback management
+                     software designed to help businesses gather, analyze, and manage customer feedback 
+                     effectively. FeedbackHub is designed to serve businesses of all sizes, including
+                      startups, small businesses, and enterprises, looking to enhance 
+                    customer engagement and improve their products and services.
+                    </p>
+                    <div class='flex justify-start md:justify-between text-lg mb-3 bg-bluegray-2 py-3 px-4'>
+                        <p>Is there a free trial available?</p>
+                        <p>-</p>
+                    </div>
+                    <p class='py-3 px-4 text-base'>Yes, we offer a free trial period for 
+                    you to explore FeedbackHub's features and see how it can benefit 
+                    your business. Simply sign up to get started!
+                    </p>
+                    <div class='flex justify-start md:justify-between text-lg mb-3 bg-bluegray-2 py-3 px-4'>
+                        <p>Who can benefit from FeedbackHub?</p>
+                        <p>-</p>
+                    </div>
+                    <p class='py-3 px-4 text-base'>FeedbackHub is designed to serve businesses of 
+                    all sizes, including startups, small businesses, and enterprises, looking to enhance customer 
+                    engagement and improve their products and services.
+                    </p>
+                    <div class='flex justify-start md:justify-between text-lg mb-3 bg-bluegray-2 py-3 px-4'>
+                        <p>Is my feedback data secure with FeedbackHub?</p>
+                        <p>-</p>
+                    </div>
+                    <p class='py-3 px-4 text-base'>Absolutely. We take data privacy and security 
+                    seriously. Feedback data is encrypted, and we follow 
+                    industry best practices to protect your information.
+                    </p>
+                    <div class='flex justify-start md:justify-between text-lg mb-3 bg-bluegray-2 py-3 px-4'>
+                        <p>Can I customize the feedback forms to match my brand identity?</p>
+                        <p>-</p>
+                    </div>
+                    <p class='py-3 px-4 text-base'>Absolutely! FeedbackHub offers customization 
+                    options to tailor feedback forms and surveys according to your brand's unique identity, 
+                    ensuring a consistent and personalized user experience.
+                    </p>
+                </div>
+            </div>
+            <div class='col-span-1 hidden md:block'>
+                <img  src={question} alt="pic"  />  
+            </div>
+            
+
         </section>
     )
 }
--- src/sections/HomePage/GetStarted.jsx
+++ src/sections/HomePage/GetStarted.jsx
@@ -1,8 +1,39 @@
+import DestructiveButton from "../../components/Button/DestructiveButton"
+import PrimaryButton from "../../components/Button/PrimaryButton";
+import SecondaryButton from "../../components/Button/SecondaryButton";
+import background from '../../assets/images/background.png'
+import form from '../../assets/images/form.png'
+import abstract from '../../assets/images/abstract.png'
+
 const GetStarted = () => {
 
     return (
-        <section className="container-108 bg-yellow-1 py-8 mb-0 lg:mb-28">
-            Get Started Section
+        <section  style={{ backgroundImage: `url(${background})`,paddingTop:'50px',marginTop:'50px' }}>
+            {/* <img src={background}  alt='logo'/> */}
+
+            <div className="  grid place-items-center mt-7 relative sm:mx-6  " >
+                <div className='bg-pri-blue py-8 sm:mx-48  mb-0 mb-28 container-64 md:container-108 fh-inner rounded-xl text-center relative '>
+                    <p class=' text-white-f9 mb-6 mt-5 text-fh-sm md:text-fh-base'>GET STARTED WITH FEEDBACK HUB FOR FREE</p>
+                    <p class='text-bluegray-9 text-fh-2xl-semi md:text-fh-4xl-semi'>Ready to Take Your Business </p>
+                    <p class='text-bluegray-9 text-fh-2xl-semi md:text-fh-4xl-semi'> to the Next Level?</p>
+                    
+                    <p class='text-blue-2 text-fh-xs-mid md:text-fh-sm-mid mt-9'>Collect, Manage, and Analyze Customer
+                        Feedback for Better Business 
+                    </p>
+                    <p class='text-blue-2 text-fh-xs-mid md:text-fh-sm-mid mb-8 '> Insights Feedback for Better Business Insights</p>
+                   
+                   
+                    <img class=' absolute bottom-0 right-0 w-64 h-52' src={form} alt="pic" />
+                    {/* <button class='rounded-xl white-f9'>get started</button> */}
+                     <PrimaryButton text='get started'/>
+                     {/* <SecondaryButton text='get started'/> */}
+                     {/* <DestructiveButton text='get stared'/> */}
+                
+                </div>
+                <img class=' absolute bottom-14 right-12 h-52 w-64 z-50' src={abstract} alt='picture'/>
+                
+            </div>
+            
         </section>
     )
 }
