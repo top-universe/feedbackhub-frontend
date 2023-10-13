120ac43a03855b23e240cbfba52f2ce0815fc5d9
Bashua Mutiat <mutiatbashua@gmail.com>
2023-10-13 21:20:56 +0100

    updated images

--- src/sections/HomePage/WhatWeProvide.jsx
+++ src/sections/HomePage/WhatWeProvide.jsx
@@ -1,29 +1,37 @@
-import cursor from "/images/cursor-icon.png";
+// import cursor from "/images/cursor-icon.png";
+import star from "../../assets/icons/star.svg";
+import people from "../../assets/icons/people.svg";
+import arrow from "../../assets/icons/arrow.svg";
+import provideImg from "../../assets/images/provide.svg";
+
+
 const WhatWeProvide = () => {
   return (
-    <section className="container-108 bg-white pt-16 pb-20">
+    <section className="container-108 bg-white pt-16 pb-20 space-y-6">
       <div className="flex flex-col sm:flex-row justify-between gap-12 items-center h-full">
-        <div className="w-full flex flex-col items-center md:items-start gap-2 md:gap-5 md:w-[55%]">
-          <p className="text-fh-xs-bold md:text-fh-base-bold text-pri-blue">WHAT WE PROVIDE</p>
+        <div className="w-full flex flex-col items-center md:items-start gap-2 md:gap-5 md:w-[55%] relative">
+          <p className="text-fh-xs-bold md:text-fh-base-bold text-pri-blue">
+            WHAT WE PROVIDE
+          </p>
           <h1 className="text-fh-xl-bold text-center md:text-left md:text-fh-5xl-bold">
             Your feedback is invaluable.
           </h1>
-          <p className="md:w-[70%] text-fh-base md:text-fh-lg text-center md:text-left">
+          <p className="md:w-[57%] text-fh-base md:text-fh-lg text-center md:text-left">
             FeedBackHub believes in giving you a platform to be heard by the
             businesses you interact with
           </p>
+          <img src={arrow} className="absolute top-[66%] left-[51%] hidden md:block" />
         </div>
         <div className="sm:w-[40%]">
-          {/* <img src={provideImg} className="" /> */}
+          <img src={provideImg} className="" />
         </div>
       </div>
 
       {/* // feedback submission */}
 
-      <div className="flex md:pt-20 flex-col md:flex-row justify-between gap-10">
+      <div className="flex pt-10 md:pt-24 flex-col md:flex-row justify-between gap-10">
         <div className="justify-between h-[100%] flex flex-col items-center md:items-stretch md:w-[30%]">
-          <img src={cursor} />
-
+          <img src={star} className="w-[10%] py-2" />
           <h3 className="text-fh-xl-bold sm:text-fh-2xl-bold">
             Easy Feedback Submission
           </h3>
@@ -32,7 +40,7 @@ const WhatWeProvide = () => {
           </p>
         </div>
         <div className="justify-between h-[100%] flex flex-col items-center md:items-stretch md:w-[30%]">
-          <div className="bg-blue-100"></div>
+          <img src={star} className="w-[10%] py-2" />
           <h3 className="text-fh-xl-bold sm:text-fh-2xl-bold">
             Discover and Rate Businesses
           </h3>
@@ -41,8 +49,8 @@ const WhatWeProvide = () => {
             from other customers' ratings and reviews.
           </p>
         </div>
-        <div className="justify-between h-[100%] flex flex-col items-center md:items-stretch md:w-[30%]">
-          <div className="bg-blue-100"></div>
+        <div className="justify-between h-[100%]  flex flex-col items-center md:items-stretch md:w-[30%]">
+          <img src={people} className="w-[10%] py-2" />
           <h3 className="text-fh-xl-bold sm:text-fh-2xl-bold">
             Connect with Businesses
           </h3>
