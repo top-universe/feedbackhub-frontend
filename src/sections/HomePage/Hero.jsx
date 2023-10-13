71c9935a046cd30d99c3a001359eb6331eea0875
Bashua Mutiat <mutiatbashua@gmail.com>
2023-10-13 20:25:37 +0100

    updated images

--- src/sections/HomePage/Hero.jsx
+++ src/sections/HomePage/Hero.jsx
@@ -1,20 +1,26 @@
+import heroImg from "../../assets/images/Reviews.svg"
+import heroBubble from "../../assets/images/bubble.svg"
 import PrimaryButton from "../../components/Button/PrimaryButton";
 const Hero = () => {
   return (
     <section className="bg-yellow-1 pt-12 pb-10 container-108">
-      <div className="flex flex-col sm:flex-row justify-between gap-12 items-center h-full">
-        <div className=" flex flex-col items-center md:items-start gap-5 md:w-[55%]">
-          <h1 className="text-[2rem] text-center md:text-left md:text-fh-5xl-bold">
+      <div className="flex flex-col sm:flex-row items-center justify-between md:items-start gap-12 h-full">
+        <div className=" flex flex-col items-center md:items-start gap-5 md:w-[55%] md:pt-10">
+          <h1 className="text-fh-2xl-bold text-center md:text-left md:text-fh-5xl-bold">
             Empower Your Feedback to Make a Difference
           </h1>
-          <p className="sm:w-[85%] text-[1.1rem] text-center md:text-left">
+          <p className="text-fh-base sm:w-[85%] md:text-fh-lg text-center md:text-left">
             With FeedBackHub, you can easily share your experiences and provide
             feedback to the products and services you love
           </p>
-          <PrimaryButton text={"Write a Review"} customClassName="text-white" />
+          <PrimaryButton text={"Write a Review"} />
         </div>
-        <div className="sm:w-[40%]">
-          {/* <img src={heroImg} className="" /> */}
+        <div className="px-10 md:p-0 sm:w-[40%] flex flex-col relative">
+          <img src={heroImg} className="" />
+          <img
+            src={heroBubble}
+            className="absolute w-[20%] right-[75%] md:w-[40%] bottom-10 md:right-[82%]"
+          />
         </div>
       </div>
     </section>
--- src/sections/HomePage/WhatWeProvide.jsx
+++ src/sections/HomePage/WhatWeProvide.jsx
@@ -1,13 +1,14 @@
+import cursor from "/images/cursor-icon.png";
 const WhatWeProvide = () => {
   return (
     <section className="container-108 bg-white pt-16 pb-20">
       <div className="flex flex-col sm:flex-row justify-between gap-12 items-center h-full">
-        <div className=" flex flex-col items-center md:items-start gap-5 md:w-[55%]">
-          <p className="text-fh-base-bold text-pri-blue">WHAT WE PROVIDE</p>
-          <h1 className=" text-[2rem] text-center md:text-left md:text-fh-5xl-bold">
+        <div className="w-full flex flex-col items-center md:items-start gap-2 md:gap-5 md:w-[55%]">
+          <p className="text-fh-xs-bold md:text-fh-base-bold text-pri-blue">WHAT WE PROVIDE</p>
+          <h1 className="text-fh-xl-bold text-center md:text-left md:text-fh-5xl-bold">
             Your feedback is invaluable.
           </h1>
-          <p className="md:w-[70%] text-fh-lg text-center md:text-left">
+          <p className="md:w-[70%] text-fh-base md:text-fh-lg text-center md:text-left">
             FeedBackHub believes in giving you a platform to be heard by the
             businesses you interact with
           </p>
@@ -19,28 +20,33 @@ const WhatWeProvide = () => {
 
       {/* // feedback submission */}
 
-      <div className="flex md:pt-20 flex-row justify-between gap-10">
-        <div className="justify-between h-[100%] flex flex-col items-stretch w-[30%]">
-          <div className="bg-blue-100"></div>
-          <h3 className="text-fh-2xl-bold">Easy Feedback Submission</h3>
-          <p className="my-2 text-fh-lg self-start">
+      <div className="flex md:pt-20 flex-col md:flex-row justify-between gap-10">
+        <div className="justify-between h-[100%] flex flex-col items-center md:items-stretch md:w-[30%]">
+          <img src={cursor} />
+
+          <h3 className="text-fh-xl-bold sm:text-fh-2xl-bold">
+            Easy Feedback Submission
+          </h3>
+          <p className="my-2 text-fh-base  md:text-fh-lg self-start text-center md:text-left">
             Share your thoughts and experiences effortlessly
           </p>
         </div>
-        <div className="justify-between h-[100%] flex flex-col items-stretch w-[30%]">
+        <div className="justify-between h-[100%] flex flex-col items-center md:items-stretch md:w-[30%]">
           <div className="bg-blue-100"></div>
-          <h3 className="text-fh-2xl-bold">
+          <h3 className="text-fh-xl-bold sm:text-fh-2xl-bold">
             Discover and Rate Businesses
           </h3>
-          <p className="my-2 text-fh-lg self-start">
+          <p className="my-2 text-fh-base  md:text-fh-lg self-start text-center md:text-left">
             Find and explore businesses in various industries. Gain insights
             from other customers' ratings and reviews.
           </p>
         </div>
-        <div className="justify-between h-[100%] flex flex-col items-stretch w-[30%]">
+        <div className="justify-between h-[100%] flex flex-col items-center md:items-stretch md:w-[30%]">
           <div className="bg-blue-100"></div>
-          <h3 className="text-fh-2xl-bold">Connect with Businesses</h3>
-          <p className="my-2 text-fh-lg self-start">
+          <h3 className="text-fh-xl-bold sm:text-fh-2xl-bold">
+            Connect with Businesses
+          </h3>
+          <p className="my-2 text-fh-base  md:text-fh-lg self-start text-center md:text-left">
             Engage directly with businesses and build meaningful relationships
           </p>
         </div>
