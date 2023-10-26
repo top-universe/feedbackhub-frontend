f8402426448eb9e764020c78af7add58766cc393
Ereyomi Funmilola <funmilolaereyomi@gmail.comgit config --global user.email funmilolaereyomi@gmail.com>
2023-10-26 13:32:55 -0700

    changes

--- src/pages/section7.jsx
+++ src/pages/section7.jsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
-
+import Section8 from './Section8';
 
 import image1 from '../assets/imagep1.png';
 import image2 from '../assets/imagep2.png';
@@ -17,7 +17,7 @@ import image9 from '../assets/imagep9.png';
 
 const Section7 = () => {
   return (
-    
+            <div>
               <div className="bg-blue-500  rounded-bl-[40px] rounded-br-[40px] mx-auto">
           <div className=" p-4 mx-auto lg:w-1440 lg:h-633.912 lg:flex-shrink-0 ">
             <div className="text-center  pt-0 px-0">
@@ -101,7 +101,8 @@ const Section7 = () => {
             </button>
             </div>
             </div>
-              
+            <Section8 />
+            </div> 
 
 
          
