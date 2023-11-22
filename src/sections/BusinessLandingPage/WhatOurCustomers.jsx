0285082aa2b0bc1849b3e1183f55cce5e587c089
Edward-Precious Omegbu <eomegbu@gmail.com>
2023-11-22 17:10:04 +0100

    masonry layout fix

--- src/sections/BusinessLandingPage/WhatOurCustomers.jsx
+++ src/sections/BusinessLandingPage/WhatOurCustomers.jsx
@@ -1,17 +1,16 @@
-import React from "react";
+// import React from "react";
 import WhatOur from "../../components/WhatOur";
   
 function WhatOurCustomers() {
   return (
-        <div className="bg-bluegray-1 p-8">
-        <div className="text-center pt-0 px-0 mb-8">
-          <h1 className="text-bluegray-9 text-center lg:text-fh-4xl-bold p-4 text-fh-2xl-semi">
-            What our customers are saying
-          </h1>
-        </div>
-        <WhatOur
-        />
-        </div>
+    <div className="bg-bluegray-1 container-108 py-20">
+      <div className="text-center pt-0 px-0 mb-12 ">
+        <h1 className="text-bluegray-9 text-center lg:text-fh-4xl-bold p-4 text-fh-2xl-semi">
+          What our customers are saying
+        </h1>
+      </div>
+      <WhatOur />
+    </div>
   );
 }
 
